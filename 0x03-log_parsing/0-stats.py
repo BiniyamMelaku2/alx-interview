3a5ebe4c82f3d85bcd6996459f082dd32c100ca0
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-12-16 21:56:04 +0300

    project description Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,27 +7,26 @@ i = 0
 val = ""
 totalsize = 0
 status = [200, 301, 400, 401, 403, 404, 405, 500]
-dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
+dict = {200: 0, 301: 0, 400: 0, 401: 0,
+        403: 0, 404: 0, 405: 0, 500: 0}
 try:
     for val in sys.stdin:
-        mylist = val.split()
-        if len(mylist) < 9:
-            continue
-        totalsize += int(mylist[-1])
-        stat = int(mylist[-2])
-        if stat in status:
-            dict[stat] += 1
         i += 1
+        mylist = val.split()
+        if len(mylist) > 2:
+            totalsize += int(mylist[-1])
+            stat = int(mylist[-2])
+            if stat in status:
+                dict[stat] += 1
         if i % 10 == 0:
             print("File size: {}".format(totalsize))
             for item in status:
-                if dict[item] != 0:
+                if dict[item]:
                     print("{}: {}".format(item, dict[item]))
 except KeyboardInterrupt:
     pass
 finally:
     print("File size: {}".format(totalsize))
     for item in status:
-        if dict[item] != 0:
+        if dict[item]:
             print("{}: {}".format(item, dict[item]))
