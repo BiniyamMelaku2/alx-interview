73083b33bf9d5a5405c32ea675e7540ddfc1fdaf
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2022-01-20 06:29:10 +0300

    rotate an n x n 2D matrix cw

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """rotate an n x n 2D matrix 90 degrees clockwise'"""
-import copy
 
 
 def rotate_2d_matrix(matrix):
     '''rotate an n x n 2D matrix 90 degrees clockwise'''
-    mxcopy = copy.deepcopy(matrix)
-    nrows = len(matrix)
-    ncols = len(matrix[0])
-    row = mxcopy[nrows-1]
-    c = 0
-    for j in range(ncols):
-        matrix[j][c] = row[j]
-    for i in range(nrows-2, -1, -1):
-        row = mxcopy[i]
-        c = c + 1
-        for j in range(ncols):
-            matrix[j][c] = row[j]
+    start = 0
+    nrow = len(matrix) - 1
+    while start < nrow:
+        for x in range(start, nrow):
+            temp = matrix[x][nrow]
+            matrix[x][nrow] = matrix[start][x]
+            temp1 = matrix[nrow][nrow + start - x]
+            matrix[nrow][nrow + start - x] = temp
+            temp = matrix[nrow + start - x][start]
+            matrix[nrow + start - x][start] = temp1
+            matrix[start][x] = temp
+        start += 1
+        nrow -= 1
